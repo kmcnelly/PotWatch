5869e19a5348da9a896c473e8ba92b3d1467bb43
Kenneth McNelly <kmcnelly@wustl.edu>
2018-04-23 11:45:42 -0500

    implementing temp

--- ui/WatchApp.js
+++ ui/WatchApp.js
@@ -2,131 +2,53 @@
 //when html loads
 document.addEventListener("DOMContentLoaded", function (){
 
-  setObserverFunc(updateUI);
-  loadLogin();
+  // setObserverFunc(updateUI);
+  loadCover();
   setup();
   // Controls Page =============================================================================
 
-  //listen for door button
-  document.getElementById("changeDoor").addEventListener("click",changeDoorStatus);
+  //listen for timer
+  document.getElementById("timerSlider").addEventListener("change",changeTimer);
 
-  //listen for light button
-  document.getElementById("changeLight").addEventListener("click",changeLight);
+  //listen for temp
+  document.getElementById("tempSlider").addEventListener("change",changeTemp);
 
-  // Advanced Features Page ====================================================================
-
-  //listen for autoClose time set button
-  document.getElementById("autoDoorCloseTimeSet").addEventListener("click", setAutoCloseTime);
-  //listen for autoClose ON button
-  document.getElementById("autoDoorCloseOn").addEventListener("click",changeAutoClose);
-  //listen for autoClose OFF button
-  document.getElementById("autoDoorCloseOff").addEventListener("click",changeAutoClose);
-
-  //listen for brightness
-  document.getElementById("lightSlider").addEventListener("change",setBrightness);
-
-  //listen for autoOff time set button
-  document.getElementById("autoOffLightTimeSet").addEventListener("click", setAutoOffTime);
 });
 
-//updates the on-screen elements based on the model
-function updateUI(updatedGarageObject){
+// updates the on-screen elements based on the model
+function updateUI(updatedWatchObject){
   // Controls Page =============================================================================
 
-  // Current Door state
-  document.getElementById("door-status").innerText = "Door " + updatedGarageObject.door.getState() + ".";
-
-  // ADD BUTTON SELECTED WHEN THE GARAGE DOOR IS OPEN OR LIGHT IS ON
-  if (updatedGarageObject.door.getState() === "open") {
-    document.getElementById("changeDoor").classList.add("button-selected");
-  } else {
-    document.getElementById("changeDoor").classList.remove("button-selected");
-  }
-
-  if (updatedGarageObject.light.getState()) {
-    document.getElementById("changeLight").classList.add("button-selected");
-  } else {
-    document.getElementById("changeLight").classList.remove("button-selected");
-  }
-
-  // Advanced Features Page ====================================================================
-
-  // fill auto close time text box ONLY IF EMPTY
-  if(document.getElementById("autoCloseTime").value == "") {
-    document.getElementById("autoCloseTime").value = updatedGarageObject.door.getAutoCloseTime();
-  }
-
-  // Auto Close Buttons
-  if (updatedGarageObject.door.getAutoCloseEnabled()) {
-    document.getElementById("autoDoorCloseOn").classList.add("button-selected");
-    document.getElementById("autoDoorCloseOff").classList.remove("button-selected");
-  } else if (!updatedGarageObject.door.getAutoCloseEnabled()) {
-    document.getElementById("autoDoorCloseOn").classList.remove("button-selected");
-    document.getElementById("autoDoorCloseOff").classList.add("button-selected");
-  }
-
-  //updates lightbrightness displayed value
-  document.getElementById("lightBrightness").innerText = updatedGarageObject.light.getBrightness() + "% Brightness.";
-
-  // fill auto off time text box ONLY IF EMPTY
-  if(document.getElementById("autoOffTime").value == ""){
-    document.getElementById("autoOffTime").value = updatedGarageObject.light.getAutoOffTime();
-  }
+  // Current time state
+  // document.getElementById("timer-status").innerText = "Time " + /*updatedWatchObject.timer.getMin() */+ ".";
 }
 
 function loadControls () {
-  document.getElementById("accountcreation").hidden = true;
   document.getElementById("controls").hidden = false;
-  document.getElementById("advancedfeatures").hidden = true;
-  document.getElementById("login").hidden = true;
+  document.getElementById("cover").hidden = true;
 }
 
-function loadAdvancedFeatures () {
-  document.getElementById("accountcreation").hidden = true;
+function loadCover () {
   document.getElementById("controls").hidden = true;
-  document.getElementById("advancedfeatures").hidden = false;
-  document.getElementById("login").hidden = true;
-}
-function loadLogin () {
-  document.getElementById("accountcreation").hidden = true;
-  document.getElementById("controls").hidden = true;
-  document.getElementById("advancedfeatures").hidden = true;
-  document.getElementById("login").hidden = false;
-}
-function loadAccountCreation () {
-  document.getElementById("accountcreation").hidden = false;
-  document.getElementById("controls").hidden = true;
-  document.getElementById("advancedfeatures").hidden = true;
-  document.getElementById("login").hidden = true;
+  document.getElementById("cover").hidden = false;
 }
 
-// open/close door
-function changeDoorStatus(){
-  garage.door.changeState();
+//updates temp displayed value
+function changeTemp(){
+  document.getElementById("Temp").innerText = document.getElementById("tempSlider").value;
 }
 
-// turn on/off light
-function changeLight(){
-  garage.light.changeState();
+//updates timer displayed value
+function changeTimer(){
+  document.getElementById("Timer").innerText = document.getElementById("timerSlider").value;
 }
 
-function changeAutoClose(){
-  if(this.value == "On"){
-    garage.door.changeAutoClose("1");
-  } else {
-    garage.door.changeAutoClose("0");
-  }
+//update the target temperature
+function setTemp(){
+  //watch.temp.setTarget(document.getElementById("tempSlider").value);
 }
 
-function setAutoCloseTime(){
-  garage.door.changeAutoCloseTime(document.getElementById("autoCloseTime").value);
-}
-
-function setAutoOffTime(){
-  garage.light.setAutoOffTime(document.getElementById("autoOffTime").value);
-}
-
-//updates brightness value of light
-function setBrightness(){
-  garage.light.setBrightness(document.getElementById("lightSlider").value);
+//update the target cook time
+function setTimer(){
+  //watch.timer.setTarget(document.getElementById("timerSlider").value);
 }
